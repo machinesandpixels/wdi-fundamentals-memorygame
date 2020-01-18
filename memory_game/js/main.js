22600c7f07f2fc2cc25f6dd51271ab1a60d91ff8
JQ <test@jq.com>
2020-01-18 15:44:36 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,23 @@
 console.log('mainJS is connected.');
 
-let cardOne = 'Queen';
-let cardTwo = 'Queen';
+const cards = ['Queen', 'Queen', 'King', 'King'];
+let cardsInPlay = [];
 
-let cardThree = 'King';
-let cardFour = 'King';
+let cardOne = cards[0];
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardOne, cardTwo);
 
 console.log(`User flipped ${cardOne}`);
+console.log(`User flipped ${cardTwo}`);
+
+if (cardsInPlay.length === 2) {
+
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    }
+
+    else {
+        alert("Sorry, try again.");
+    }
+}
